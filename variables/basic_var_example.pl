47f98f68f43fd62ce02fd8cfbb63bd179a36658a
danny.gurman <danny.gurman@gmail.com>
2020-01-20 19:00:13 +0200

    init

---
+++ variables/basic_var_example.pl
@@ -0,0 +1,16 @@
+#! perl
+
+#use warnings;
+#use strict;
+$x = 10;
+$y = 20;
+$s = "Perl string";
+print($s);
+
+#In Perl, an expression is anything that returns a value
+#A statement is made up of expressions. Statement is executed by Perl at run-time.
+#  Each Perl statement must end with a semicolon (;).
+$a = 1;
+$b = 2;
+$c = $a + $b;
+print($c);
